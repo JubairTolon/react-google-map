c968dc094362659b35ab6baac205edc1f7555b7f
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-05-29 17:34:50 +0600

    my location component create

---
+++ src/Components/MyLocation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+function MyComponent() {
+    // code[] copy from google_map_api
+    return (
+        <LoadScript
+            // witgout API_Key it's give error
+            googleMapsApiKey="YOUR_API_KEY"
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                { /* Child components, such as markers, info windows, etc. */}
+                <></>
+            </GoogleMap>
+        </LoadScript>
+    )
+}
+
+export default React.memo(MyComponent)
